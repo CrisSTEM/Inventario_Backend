be709b3439bb6de36f824ba760613d0bdecf6e71
An Garcia <cristianagguniversity@gmail.com>
2023-12-07 19:52:17 -0600

    Definicion de rutas en api.php

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\VentaController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
+// Rutas existentes
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
@@ -21,3 +15,31 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/hola', function () {
     return response()->json(['mensaje' => 'Hola Mundo']);
 });
+
+// ClienteController
+Route::get('/clientes', [ClienteController::class, 'index']);
+Route::post('/clientes', [ClienteController::class, 'store']);
+Route::get('/clientes/{id}', [ClienteController::class, 'show']);
+Route::put('/clientes/{id}', [ClienteController::class, 'update']);
+Route::delete('/clientes/{id}', [ClienteController::class, 'destroy']);
+
+// ProductoController
+Route::get('/productos', [ProductoController::class, 'index']);
+Route::post('/productos', [ProductoController::class, 'store']);
+Route::get('/productos/{id}', [ProductoController::class, 'show']);
+Route::put('/productos/{id}', [ProductoController::class, 'update']);
+Route::delete('/productos/{id}', [ProductoController::class, 'destroy']);
+
+// UsuarioController
+Route::get('/usuarios', [UsuarioController::class, 'index']);
+Route::post('/usuarios', [UsuarioController::class, 'store']);
+Route::get('/usuarios/{id}', [UsuarioController::class, 'show']);
+Route::put('/usuarios/{id}', [UsuarioController::class, 'update']);
+Route::delete('/usuarios/{id}', [UsuarioController::class, 'destroy']);
+
+// VentaController
+Route::get('/ventas', [VentaController::class, 'index']);
+Route::post('/ventas', [VentaController::class, 'store']);
+Route::get('/ventas/{id}', [VentaController::class, 'show']);
+Route::put('/ventas/{id}', [VentaController::class, 'update']);
+Route::delete('/ventas/{id}', [VentaController::class, 'destroy']);
